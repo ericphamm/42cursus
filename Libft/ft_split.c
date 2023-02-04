f2c0ed2857f83a03605c3de19068cb2aa95e1a1a
ericphamm <phamwangthang@gmail.com>
2023-02-04 21:03:18 +0100

    ft_split

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -5,8 +5,70 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: qupham <qupham@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/29 22:14:17 by qupham            #+#    #+#             */
-/*   Updated: 2023/01/29 22:14:23 by qupham           ###   ########.fr       */
+/*   Created: 2023/02/04 18:36:13 by qupham            #+#    #+#             */
+/*   Updated: 2023/02/04 20:50:39 by qupham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+static int	array_count(const char *s, char c)
+{
+	int	num;
+	int	i;
+
+	num = 0;
+	i = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i])
+		{
+			num++;
+			while (s[i] != c && s[i])
+				i++;
+		}
+	}
+	return (num);
+}
+
+static char	*allocate(int i, int j, const char *s)
+{
+	char	*res;
+
+	res = (char *)malloc((j + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	ft_strlcpy(res, &s[*i], j + 1);
+	*i = *i + j;
+	return (res);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**res;
+	int		i;
+	int		j;
+	int		ar;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	res = (char **)malloc((array_count(s, c) + 1) * sizeof(char *));
+	if (!res)
+		return (NULL);
+	ar = 0;
+	while (s[i])
+	{
+		j = 0;
+		while (s[i] == c)
+			i++;
+		while (s[i + j] && s[i + j] != c)
+			j++;
+		if (j > 0)
+			res[ar++] = allocate(&i, j, s);
+	}
+	res[ar] = NULL;
+	return (res);
+}
